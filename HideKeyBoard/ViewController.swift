f3f1ab55315d9044cb21487026811708aea287c9
Jordan Fritzen <s0419087@student.rockvalleycollege.edu>
2020-09-27 06:37:26 -0700

    Initial Commit

---
+++ HideKeyBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HideKeyBoard
+//
+//  Created by user179545 on 9/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
